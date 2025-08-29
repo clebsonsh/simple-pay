c1b9558693f1bfca90af782ddc9a62cd2ee46dc7
Clebson Moura <clebson.sh@gmail.com>
2025-08-29 00:19:09 -0300

    feat: add router for transfers

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\V1\TransferController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return ['name' => 'john doe'];
-});
+Route::post('/v1/transfer', TransferController::class);
