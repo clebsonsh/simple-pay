781f35b4287c73053278c878520df0ff2bd69beb
Clebson Moura <clebson.sh@gmail.com>
2025-08-29 22:02:47 -0300

    feat: add AuthorizationService

---
+++ app/Services/Api/V1/AuthorizationService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Services\Api\V1;
+
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Http\Client\PendingRequest;
+use Illuminate\Support\Facades\Http;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthorizationService
+{
+    private PendingRequest $httpClient;
+
+    public function __construct()
+    {
+        /** @var string $url */
+        $url = config('services.authorization_service.url');
+
+        $this->httpClient = Http::baseUrl($url);
+    }
+
+    /** @throws ConnectionException */
+    public function check(): bool
+    {
+        $response = $this->httpClient->get('authorize');
+        $statusCode = $response->status();
+
+        return $statusCode === Response::HTTP_OK;
+    }
+}
