0e28339a0869da8f48ab70026ad3a239cd8b07ea
Clebson Moura <clebson.sh@gmail.com>
2025-09-03 23:05:43 -0300

    refactor: simplify AuthorizationService to follow the same structure of NotifyPayee

--- app/Services/Api/V1/AuthorizationService.php
+++ app/Services/Api/V1/AuthorizationService.php
@@ -3,26 +3,18 @@
 namespace App\Services\Api\V1;
 
 use Illuminate\Http\Client\ConnectionException;
-use Illuminate\Http\Client\PendingRequest;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Http;
 
 class AuthorizationService
 {
-    private PendingRequest $httpClient;
-
-    public function __construct()
+    /** @throws ConnectionException */
+    public function check(): bool
     {
         /** @var string $url */
         $url = config('services.authorization_service.url');
 
-        $this->httpClient = Http::baseUrl($url);
-    }
-
-    /** @throws ConnectionException */
-    public function check(): bool
-    {
-        $response = $this->httpClient->get('authorize');
+        $response = Http::baseUrl($url)->get('authorize');
         $statusCode = $response->status();
 
         return $statusCode === Response::HTTP_OK;
