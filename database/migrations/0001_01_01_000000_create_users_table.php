fe448c54074d8098f7982012df6a9781fb33216d
Clebson Moura <clebson.sh@gmail.com>
2025-09-03 21:37:22 -0300

    refactor: add UserType Enum

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\UserType;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,7 +16,7 @@ return new class extends Migration
             $table->uuid('id')->primary();
             $table->string('name', 120);
             $table->string('cpf_cnpj', 14)->unique();
-            $table->enum('type', ['customer', 'merchant']);
+            $table->enum('type', UserType::cases());
             $table->string('email', 120)->unique();
             $table->unsignedBigInteger('balance');
             $table->string('password', 255);
